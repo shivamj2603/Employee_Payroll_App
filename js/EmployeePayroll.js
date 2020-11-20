92b39b78b1f2a5527a2efaf8e5ee531132320b3b
shivamj2603 <shivamj2603@gmail.com>
2020-11-20 18:08:51 +0530

    Added:Event Listener on Salary Range to display appropriate value

---
+++ js/EmployeePayroll.js
@@ -0,0 +1,8 @@
+window.addEventListener('input', function(){
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function(){
+        output.textContent = salary.value;
+    }); 
+}); 
